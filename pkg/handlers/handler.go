0d21b10256764772cf70357c1edfb30c68357b91
unknown <mrph14@gmail.com>
2022-06-18 00:55:46 +0430

    enabling go modules and refactoring

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -1,15 +1,17 @@
-package main
+package handlers
 
 import (
 	"net/http"
+
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/render"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.html")
+	render.RenderTemplate(w, "home.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.html")
+	render.RenderTemplate(w, "about.html")
 }
