4b32bc5c68b4928a0718d052d7c512f6acff5649
MohammadJavad14 <mrph14@gmail.com>
2022-06-18 22:48:44 +0430

    sharing data with templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/MohammadJavad14/Room-Reservation.git/pkg/config"
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/models"
 	"github.com/MohammadJavad14/Room-Reservation.git/pkg/render"
 )
 
@@ -25,10 +26,14 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home-page.html")
+	render.RenderTemplate(w, "home-page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about-page.html")
+	stringMap := make(map[string]string)
+	stringMap["test"] = " Hello, again."
+	render.RenderTemplate(w, "about-page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"github.com/MohammadJavad14/Room-Reservation.git/pkg/config"
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/models"
 )
 
 var functions = template.FuncMap{}
@@ -20,7 +21,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the template cache from the app config
@@ -34,7 +40,10 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		log.Fatal("Could not get template from template cache")
 	}
 	buf := new(bytes.Buffer)
-	_ = t.Execute(buf, nil)
+
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
 
 	_, err := buf.WriteTo(w)
 	if err != nil {
