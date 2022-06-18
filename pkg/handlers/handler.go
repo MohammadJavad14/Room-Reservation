e0a9c1dbc1fb8f8b9f5b06426834ccc50d257e6f
MohammadJavad14 <mrph14@gmail.com>
2022-06-18 20:35:58 +0430

    building a template cache

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -8,10 +8,10 @@ import (
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.html")
+	render.RenderTemplate(w, "home-page.html")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.html")
+	render.RenderTemplate(w, "about-page.html")
 }
