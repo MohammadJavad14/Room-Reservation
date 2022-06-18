dcca440d51f857d0a298aff9ed48895a12f6f37b
MohammadJavad14 <mrph14@gmail.com>
2022-06-18 21:11:48 +0430

    use app config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/config"
 	"github.com/MohammadJavad14/Room-Reservation.git/pkg/handlers"
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -3,15 +3,32 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/MohammadJavad14/Room-Reservation.git/pkg/config"
 	"github.com/MohammadJavad14/Room-Reservation.git/pkg/render"
 )
 
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home-page.html")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about-page.html")
 }
